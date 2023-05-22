6a0e98d8c7f9bea72c96894ed7ceee1129c464ba
Matheus Lamana <matheus.lamana1@outlook.com>
2023-05-22 17:54:31 -0300

    Commit Inicial

---
+++ server.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const express = require('express');  //como ele ja esta dentro da pasata node a gente nao precisa de caminho para importar
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.emit('Pronto');
+    }).catch(e => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware.js');
+
+const helmet = require('helmet');
+const csrf = require('csurf');
+
+app.use(express.urlencoded({ extended: true })); //se eu nao usar qualquer coisa da url vai me retornar undefined
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'podeserqualquercoisa',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+
+app.use(helmet());
+app.use(sessionOptions);
+app.use(flash());
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(csrf());
+
+//Nossos proprios Middlewares
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on('Pronto',()=> {
+    app.listen(3000, () => {
+        console.log('Acessar: http://localhost:3000');
+    });
+});
+
+
